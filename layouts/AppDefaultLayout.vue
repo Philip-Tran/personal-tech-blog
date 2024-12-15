f7cda710a872f8859bf6a059f492ca23f63ea682
Quyet's Code <quyetcoccoc@gmail.com>
2024-12-15 10:26:03 +0700

    init, setup dependencies

---
+++ layouts/AppDefaultLayout.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <h1>haha</h1>
+    <slot />
+  </div>
+</template>
+
+<script lang="ts" setup>
+
+</script>
+
+<style></style>
---
+++ pages/index.vue
@@ -0,0 +1,9 @@
+<template>
+    <div>
+        <Button>Click me</Button>
+        <h1 class="text-red-500"> Welcome to the homepage</h1>
+        <AppAlert>
+            This is an auto-imported component
+        </AppAlert>
+    </div>
+</template>
