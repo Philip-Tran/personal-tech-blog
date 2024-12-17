a4acefe596282e2aee9a8450cd0e41dfb68b02ad
Quyet's Code <quyetcoccoc@gmail.com>
2024-12-17 13:36:18 +0700

    adjust cron job func

--- server/api/keepalive.ts
+++ server/api/keepalive.ts
@@ -2,10 +2,13 @@ import { prisma } from "~/lib/prisma";
 
 export const defineEventHandler = async () => {
   try {
-    const posts = await prisma.post.findMany();
-
+    const posts = await prisma.post.findMany({
+      take: 3,
+    });
+    console.log("Ping by cron job success");
     return { message: "Database pinged successfully", posts: posts };
   } catch (error) {
+    console.log("Ping by cron job fail");
     return { Error: (error as Error).message };
   }
 };
