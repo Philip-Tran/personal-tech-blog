01199b86a1ac1aa66717e1bbb6f92bbd8ea418eb
Quyet's Code <quyetcoccoc@gmail.com>
2025-01-18 22:37:56 +0700

    [feat] add facebook share button

--- pages/(client)/[slug].vue
+++ pages/(client)/[slug].vue
@@ -11,6 +11,7 @@ import { getReadingTime, formatDate } from "~/lib/utils";
 import { type Post } from "~/stores/client/PostStore";
 import { usePostStore } from "~/stores/client/PostStore";
 
+import { Facebook } from "lucide-vue-next";
 import { useRoute } from "vue-router";
 
 const postStore = usePostStore();
@@ -67,6 +68,14 @@ watch(post, () => {
         })
     }
 }, { immediate: true })
+
+
+// social share
+const shareToFacebook = () => {
+    const shareUrl = encodeURIComponent("http://phil-tran-blog.vercel.app");
+    const facebookUrl = `https://www.facebook.com/sharer/sharer.php?u=${shareUrl}`;
+    window.open(facebookUrl, "_blank");
+}
 </script>
 
 <template>
@@ -85,11 +94,22 @@ watch(post, () => {
     <!-- Main -->
     <article v-else class="lg:max-w-[700px] my-8 lg:my-12 self-center lg:mx-auto p-6">
         <h1 class="text-3xl lg:text-4xl font-bold mb-2 xl:mb-3">{{ post.title }}</h1>
-        <div class="text-sm text-yellow-800 lg:text-base font-medium mb-6 lg:mb-10 xl:mb-12 2xl:mb-14">
+        <div class="text-sm text-yellow-800 lg:text-base font-medium">
             <span class="">{{ formatDate(post.createdAt) }}</span>
             <span class="ml-4"> {{ getReadingTime(post.content) }} </span>
         </div>
-        <div class="text-[#363737] mb-2 font-serif text-[18px] lg:text-[19px] leading-9 prose prose-h1:font-sans prose-h2:font-sans prose-h3:font-sans prose-h4:font-sans prose-h5:font-sans"
-            v-html="post.content"></div>
+        <div class="w-full border-y my-6">
+            <div class="flex space-x-3 lg:space-x-4 items-center my-3">
+                <span class="font-bold">Share: </span>
+                <div>
+                    <Button @click="shareToFacebook" variant="outline" class="rounded-full w-10 h-10">
+                        <Facebook />
+                    </Button>
+                </div>
+            </div>
+        </div>
+        <div class="text-[#363737] mb-2 mt-6 lg:mt-10 xl:mt-12 2xl:mt-14  font-serif text-[18px] lg:text-[19px] leading-9 prose prose-h1:font-sans prose-h2:font-sans prose-h3:font-sans prose-h4:font-sans prose-h5:font-sans"
+            v-html="post.content">
+        </div>
     </article>
 </template>
